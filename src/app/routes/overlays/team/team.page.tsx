9fe4a80651b9a7389ff9cf485f0a472ad94020c9
jasper <jasper@manok.dev>
2022-12-08 05:51:47 +0800

    chore: lint

--- src/app/routes/overlays/team/team.page.tsx
+++ src/app/routes/overlays/team/team.page.tsx
@@ -1,9 +1,6 @@
 import { Group } from "@mantine/core";
-import { useMatch } from "@tanstack/react-router";
 
 const TeamPage = () => {
-  const { params } = useMatch("/overlays/team/$side");
-  const { side } = params;
   return <Group noWrap>test</Group>;
 };
 
